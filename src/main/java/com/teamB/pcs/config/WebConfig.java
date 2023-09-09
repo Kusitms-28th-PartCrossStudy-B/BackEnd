3e0c61e9446571fed6ef8c536c54c8ddc78a3573
kyeong-hyeok <minkh4227@naver.com>
2023-09-09 17:14:38 +0900

    chore: CORS 에러 설정

---
+++ src/main/java/com/teamB/pcs/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.teamB.pcs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .maxAge(3000);
+
+    }
+}
